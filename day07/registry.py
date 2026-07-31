0d1adabfa83951d649fc845c24131866c6d38d2e
dwa006028-beep <dwa006o28@gmail.com>
2026-07-31 06:48:26 -0700

    Update Day 7 account registry

--- day07/registry.py
+++ day07/registry.py
@@ -2,12 +2,22 @@ class AccountRegistry:
 
     def __init__(self):
         self.accounts = {}
+
     def add(self, account):
         self.accounts[account.account_number] = account
+
     def find(self, number):
         return self.accounts.get(number)
+
     def list_all(self):
         return sorted(
             self.accounts.values(),
             key=lambda acc: acc.account_number
         )
+acc1=Account("Dawa","CBE-001",1500)
+
+registry = AccountRegistry()
+
+registry.add(acc1)
+
+print(registry.find("CBE-001").owenerS)
