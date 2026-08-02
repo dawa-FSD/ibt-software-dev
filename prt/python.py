9ff089c29e0d4b94eaf34fe9a944096131fb6503
dwa006028-beep <dwa006o28@gmail.com>
2026-08-02 03:36:16 -0700

    add practce data type

---
+++ prt/python.py
@@ -0,0 +1,8 @@
+a='dawa asfaw dusdiuosdkjhfsdhocdschgsiosjchiusdchjsodichosckhksiduvkd'
+print(a)
+int={1,2,3,4,5,6}
+li=(7,2,27,383,"dawa asafaw")
+print(int)
+print(li)
+pin=int(input("PLESE Enter your pin number"))
+print( "your pin number is " + pin)
